5153e1341206c6e96f6ed41361a5feb59112d509
moyuxing <moyuxing@kp.com>
2024-05-30 17:47:13 +0800

    feat(feign): 引入feign

--- itmuch-boot/it-content-center/src/main/java/com/itmuch/ContentCenterApplication.java
+++ itmuch-boot/it-content-center/src/main/java/com/itmuch/ContentCenterApplication.java
@@ -6,10 +6,12 @@ import com.netflix.loadbalancer.RandomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ContentCenterApplication {
 
     public static void main(String[] args) {
--- itmuch-boot/it-user-center/src/main/java/com/itmuch/UserCenterApplication.java
+++ itmuch-boot/it-user-center/src/main/java/com/itmuch/UserCenterApplication.java
@@ -2,8 +2,10 @@ package com.itmuch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class UserCenterApplication {
 
     public static void main(String[] args) {
